dbbf6bb7ecbfd665e2c4afabfadce86d9e2f8519
Parker <Parker.engelson09@gmail.com>
2026-04-02 08:16:08 -0700

    Set up multiple rows of multicolored bricks

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -116,11 +116,15 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         // now, figure the number and spacing of each row of bricks
         let count = Int(frame.width) / 55
         let xOffset = (Int(frame.width) - (count * 55)) / 2 + Int(frame.minX) + 25
-        let y = Int(frame.maxY) - 65
-        for i in 0..<count
+        let colors: [UIColor] = [.blue, .orange, .green]
+        for r in 0..<3
         {
-            let x = i * 55 + xOffset
-            makeBrick(x: x, y: y, color: .green)
+            let y = Int(frame.maxY) - 65 - (r * 25)
+            for i in 0..<count
+            {
+                let x = i * 55 + xOffset
+                makeBrick(x: x, y: y, color: colors[r])
+            }
         }
     }
     func makeLoseZone()
@@ -190,14 +194,22 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     func didBegin(_ contact: SKPhysicsContact) {
         // ask eack brick, "Is it you?"
         for brick in bricks {
-            if contact.bodyA.node?.name == "brick" ||
-                contact.bodyB.node?.name == "brick" {
+            if contact.bodyA.node == brick ||
+                contact.bodyB.node  == brick {
                 score += 1
                 updateLabels()
-                brick.removeFromParent()
-                removedBricks += 1
-                if removedBricks == bricks.count {
-                    gameOver(winner: true)
+                if brick.color == .blue {
+                    brick.color = .orange
+                }
+                else if brick.color == .orange {
+                    brick.color = .green
+                }
+                else {
+                    brick.removeFromParent()
+                    removedBricks += 1
+                    if removedBricks == bricks.count {
+                        gameOver(winner: true)
+                    }
                 }
             }
         }
